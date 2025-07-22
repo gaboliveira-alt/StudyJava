8ebf618ee90a3cac8f81e061c857a5a2fb1d6da8
Gabriel Oliveira Pinto <157530699+gaboliveira-alt@users.noreply.github.com>
2025-07-22 17:04:31 -0300

    iniciando Java

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Argumento 1: " + args[0]);
+        System.out.println("Argumento 2: " + args[1]);
+    }
+}
